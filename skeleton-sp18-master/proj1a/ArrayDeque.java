38ca3f543fa9f32e381c91fd6269a1febdbdb4ff
yy2540 <81672984+yy2540@users.noreply.github.com>
2021-05-28 15:17:02 +0800

    complete project1a

---
+++ skeleton-sp18-master/proj1a/ArrayDeque.java
@@ -0,0 +1,122 @@
+public class ArrayDeque<T> {
+    private T[] items;
+    private int size;
+    private int nextFirst;
+    private int nextLast;
+
+    public ArrayDeque(){
+        items = (T[]) new Object[8];
+        size = 0;
+        nextFirst = 0;
+        nextLast = 1;
+    }
+
+    /**
+     * 将双端队列中数的下一个数的位置映射到实际的数组中。
+     * 例如，这一个数索引为items.length-1（即数组最后一位），则下一个数
+     * 应该位于0位置.若索引还不到最后一位，则下一个数的位置
+     * 就是下一个数的索引，正常+1即可。
+     * <p>
+     * 这个trick应该很常用于循环。每次将索引+1，一到某个阈值
+     * 就从头开始。
+     *
+     * @param a
+     * @return
+     */
+    private int addOne(int a) {
+        return (a + 1) % items.length;
+    }
+
+    private int subOne(int a) {
+        return (a - 1 + items.length) % items.length;
+    }
+
+    /**
+     * resize the array.
+     * 主要思想是，将原数组从first到end复制新数组的0到size中。
+     * 则新数组的nextFirst为新数组的最后一个，新数组的nextLast为新数组的size索引。
+     *
+     * @param length
+     */
+    private void resize(int length) {
+        T[] newItems = (T[]) new Object[length];
+        int oldindex = addOne(nextFirst);
+        for (int i = 0; i < size; i++) {
+            newItems[i] = items[oldindex];
+            oldindex = addOne(oldindex);
+        }
+        this.items = newItems;
+        nextFirst = items.length - 1;
+        nextLast = size;
+    }
+
+    public void addFirst(T item){
+        if (size == items.length){  //数组满了要扩容
+            resize(2 * size);
+        }
+        items[nextFirst] = item;
+        nextFirst = subOne(nextFirst);
+        size++;
+    }
+
+    public void addLast(T item){
+        if (size == items.length){  //数组满了要扩容
+            resize(2 * size);
+        }
+        items[nextLast] = item;
+        nextLast = addOne(nextLast);
+        size++;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public void printDeque(){
+        for (int i = 0; i < items.length; i++){
+            if (items[i] == null){
+                continue;
+            }
+            System.out.print(items[i] + " ");
+        }
+    }
+
+    public T removeFirst(){
+        if (size == 0){
+            return null;
+        }
+        T rm = items[addOne(nextFirst)];
+        items[addOne(nextFirst)] = null;
+        nextFirst = addOne(nextFirst);
+        size--;
+        if (items.length >= 16 && size < (items.length) / 4){   //数组密度不到1/4要减少容量
+            resize(items.length / 2);
+        }
+        return rm;
+    }
+
+    public T removeLast(){
+        if (size == 0){
+            return null;
+        }
+        T rm = items[subOne(nextLast)];
+        items[subOne(nextLast)] = null;
+        nextLast = subOne(nextLast);
+        size--;
+        if (items.length >= 16 && size < (items.length) / 4){   //数组密度不到1/4要减少容量
+            resize(items.length / 2);
+        }
+        return rm;
+    }
+
+    public T get(int index){
+        if (index >= items.length){ //目标索引超出数组长度
+            return null;
+        }
+        return items[index];
+    }
+}
--- skeleton-sp18-master/proj1a/LinkedListDeque.java
+++ skeleton-sp18-master/proj1a/LinkedListDeque.java
@@ -1,3 +1,115 @@
-public class LinkedListDeque {
-    
+public class LinkedListDeque<T> {
+    private class ItemNode{
+        private T item;
+        private ItemNode pre;
+        private ItemNode next;
+
+        public ItemNode(T item, ItemNode pre, ItemNode next){
+            this.item = item;
+            this.pre = pre;
+            this.next = next;
+        }
+    }
+
+    private int size;
+    private ItemNode sentinel;
+
+    public LinkedListDeque(){
+        /*
+        循环链表与哨兵节点：在双向链表中设置一个sentinel，它的next永远指向第一个节点，pre永远指向最后一个节点
+        （即使此时链表为空，只有它自己，那么next和pre就都指向自己）。
+         */
+        sentinel = new ItemNode(null, null, null);
+        sentinel.next = sentinel;
+        sentinel.pre = sentinel;
+    }
+
+    public void addFirst(T item){
+        ItemNode a = new ItemNode(item, sentinel, sentinel.next);
+        sentinel.next.pre = a;
+        sentinel.next = a;
+        size++;
+    }
+
+    public void addLast(T item){
+        ItemNode a = new ItemNode(item, sentinel.pre, sentinel);
+        sentinel.pre.next = a;
+        sentinel.pre = a;
+        size++;
+    }
+
+    public boolean isEmpty(){
+        /*
+        Internet:
+        return sentinel.next == sentinel;
+         */
+        if (size == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public void printDeque(){
+        ItemNode cur = sentinel;
+        while(cur.next != sentinel){
+            System.out.println(cur.next.item + " ");
+            cur = cur.next;
+        }
+    }
+
+    public T removeFirst(){
+        if (size == 0){
+            return null;    //empty deque
+        }else{
+            T r = sentinel.next.item;
+            sentinel.next.next.pre = sentinel;
+            sentinel.next = sentinel.next.next;
+            size--;
+            return r;
+        }
+    }
+
+    public T removeLast(){
+        if (size == 0){
+            return null;
+        }else{
+            T r = sentinel.pre.item;
+            sentinel.pre.pre.next = sentinel;
+            sentinel.pre = sentinel.pre.pre;
+            size--;
+            return r;
+        }
+    }
+
+    public T get(int index){
+        if (index >= size){ // index out of bound
+            return null;
+        }
+        ItemNode cur = sentinel;
+        for (int i = 0; i < index - 1; i++){
+            cur = cur.next;
+        }
+        return cur.item;
+    }
+
+    public T getRecursive(int index){
+        if (index >= size){ //index out of bound
+            return null;
+        }
+        return getRecursiveHelper(sentinel, index);
+    }
+
+    public T getRecursiveHelper(ItemNode node, int n){
+        // basis case
+        if (n == 0){
+            return node.item;
+        }
+        // recursive case
+        return getRecursiveHelper(node, n - 1);
+    }
 }
